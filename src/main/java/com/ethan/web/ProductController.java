57c8cf4df221e919efce9ab6cdcc1e4371f06c6c
Lingxin Gu <lingxingu0112@gmail.com>
2017-02-28 23:34:32 +0800

    add devtools to pom and show current time in the page

--- src/main/java/com/ethan/web/ProductController.java
+++ src/main/java/com/ethan/web/ProductController.java
@@ -1,8 +1,13 @@
 package com.ethan.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by lingxingu on 2017/2/27.
@@ -11,7 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     @RequestMapping("/")
-    public String home() {
+    public String home(Model model) {
+        Calendar now = Calendar.getInstance();
+        Date date = now.getTime();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        model.addAttribute("today", dateFormat.format(date));
         return "home";
     }
 }
